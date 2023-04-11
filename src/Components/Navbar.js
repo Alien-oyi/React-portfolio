829506af9e2eda20f34079ef0f501199d67f17ff
Alien-oyi <daijinmin78@gmail.com>
2023-04-10 23:04:39 -0400

    add navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+export default function navbar() {
+  return (
+    <div>
+        <nav className="navbar navbar-expand-lg ">
+            <div className="container-fluid ">
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav" >
+                        <a className="nav-link active" aria-current="page" href="#" alt="Home">Home</a>
+                        <a className="nav-link" href="#" alt="Profolio">Profolio</a>
+                        <a className="nav-link" href="#" alt="Contact">Contact</a>
+                        <a className="nav-link" href="#" alt="Resume">Resume</a>
+                    </div>
+                </div>
+            </div>
+        </nav>      
+    </div>
+  )
+}
+

4e9ea1b9a4b8c2e266c6ae302b951d951c737cca
Alien-oyi <daijinmin78@gmail.com>
2023-04-12 20:07:35 -0400

    update build

--- src/Components/Pages/Portfolio.js
+++ src/Components/Pages/Portfolio.js
@@ -1,10 +1,35 @@
+import '../Styles/Portfolio.css'
+import groupiegang from '../../Assets/groupiehero_720.png'
+import historwe from '../../Assets/HistorWe.png'
 export default function Portfolio() {
-     return (
-        <div>
-            <h1>Portfolio Page</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            </p>
+    return (
+        <>
+        <div className="card-container">
+            <div className="image-container">
+                <a href=''>
+                    <img src={groupiegang} alt="project" />
+                </a>
+            </div>
+            <div className="card-title">
+                <h3>loreum ipsum
+                </h3>
+            </div>
+            <div className="card-body">
+                <p>aadsfasdfs</p>
+            </div>
         </div>
+                <div className="card-container">
+            <div className="image-container">
+                <img src={historwe} alt="project" />
+            </div>
+            <div className="card-title">
+                <h3>loreum ipsum
+                </h3>
+            </div>
+            <div className="card-body">
+                <p>aadsfasdfs</p>
+            </div>
+        </div>
+    </>
     )
 }

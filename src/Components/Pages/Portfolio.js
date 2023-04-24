376e29366464204d84e873c2e0a4b2882a9391af
Alien-oyi <daijinmin78@gmail.com>
2023-04-24 13:30:27 -0400

    add gamebox

--- src/Components/Pages/Portfolio.js
+++ src/Components/Pages/Portfolio.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../Styles/Portfolio.css';
 import groupiegang from '../../Assets/groupiehero_720.png';
 import historwe from '../../Assets/HistorWe.png';
+import gamebox from '../../Assets/gamebox.png';
 
 export default function Portfolio() {
   return (
@@ -30,6 +31,27 @@ export default function Portfolio() {
               </p>
             </div>
           </div>
+          <div className="row">
+          <div className="col-md-6">
+          <div className="card mb-3">
+            <a href="https://gameboxed.herokuapp.com/" alt="project">
+              <img src={gamebox} className="card-img-top" alt="project" />
+            </a>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="card mb-3">
+            <div className="card-body">
+              <h2 className="card-title">Groupie-Gang</h2>
+              <p className="card-text">
+Gamebox is a web-based application built using React that allows users to browse and play a variety of games while also providing a personalized experience. The app provides a platform for users to create and customize their profiles, save games they have played, and search for new games using the RAWG API.
+Upon visiting the Gamebox website, users will be prompted to create a profile where they can upload a profile picture, set a username, and add a brief bio. Once logged in, users can browse through a variety of games and save their favorite games to their profile. The app also allows users to keep track of their progress in games they have played and mark games as completed.
+              </p>
+            </div>
+          </div>
+          </div>
+          </div>
+          <div className="row">
           <div className="card mb-3">
             <div className="card-body">
               <h2 className="card-title">HistorWe</h2>
@@ -47,6 +69,7 @@ export default function Portfolio() {
             <a href="https://calm-castle-61617.herokuapp.com/" alt="project">
               <img src={historwe} className="card-img-top" alt="project" />
             </a>
+          </div>
           </div>
         </div>
       </div>
